b6489726c0772276968a8fbaadbff59412b8ccda
Ushel <utkarsh03@icloud.com>
2025-04-20 00:20:14 +0530

    LLM Langchain Framework

---
+++ project/LLM/chatbot/llamaapp.py
@@ -0,0 +1,11 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ[""]=os.getenv()
+os.environ[""]=os.getenv()
